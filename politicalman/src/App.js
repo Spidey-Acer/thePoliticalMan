f5a90c9be8f1e97d65d0ffea18d7feb2d0377f4d
Peter-Kibet <peter.kibet@hotmail.com>
2023-02-13 09:03:26 +0300

    Skeleton of thePolitical man

---
+++ politicalman/src/App.js
@@ -0,0 +1,14 @@
+// ./src/App.js
+
+import React from 'react';
+import Home from './pages/home/Home.jsx';
+
+function App() {
+    return (
+        <div className="App">
+            <Home />
+        </div>
+    );
+}
+
+export default App;
---
+++ politicalman/src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Home.scss"
+
+const Home = () => {
+  return (
+    <div className="home">
+        <div className="homeWrapper">homepage</div>
+    </div>
+  )
+}
+
+export default Home
